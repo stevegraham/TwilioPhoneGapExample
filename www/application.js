5824d64d49e140401344d4acb592374b0c43f985
Stevie Graham <sjtgraham@mac.com>
2012-03-27 19:03:26 +0100

    hello world

--- www/application.js
+++ www/application.js
@@ -1,3 +1,13 @@
 document.addEventListener("touchmove", function(ev) { ev.preventDefault() }, false);
 
-document.addEventListener("deviceready", onDeviceReady, false);
+document.addEventListener("deviceready", function(ev) {
+
+  var tokenRequest    = new XMLHttpRequest();
+  tokenRequest.onload = function() {
+    Twilio.Device.setup(tokenRequest.responseText);
+    Twilio.Device.connect({room: 'phonegap' })
+  };
+
+  tokenRequest.open('GET', 'http://yob.ro/token');
+  tokenRequest.send();
+}, false);
